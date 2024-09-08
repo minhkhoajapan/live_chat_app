27a22c07ef9607e0b2a34e9d32c95750ee7e43af
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-09 05:04:36 +0900

    add Message model in backend/chat/models.py

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+    message = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    room_name = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
