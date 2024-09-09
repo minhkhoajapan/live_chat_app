182114865ce75fcda5e379249bde5482f227713f
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-09 22:49:15 +0900

    add update_message_db in chat/consumers.py to save message each time sent

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,6 +1,7 @@
 import json
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from chat.serializers import MessageSerializer
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
@@ -19,16 +20,30 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def receive(self, text_data):
         text_data_jason = json.loads(text_data)
         message = text_data_jason["message"]
-        sender = text_data_jason.get("sender", "Anonymous")
+        sender_name = text_data_jason.get("sender_name", "Anonymous")
+        room_name = self.room_name
+
+        self.update_message_db(message=message, sender_name=sender_name, room_name=room_name)
 
         #Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message, "sender": sender}
+            self.room_group_name, {"type": "chat.message", "message": message, "sender_name": sender_name, "room_name": room_name}
         )
  
     async def chat_message(self, event):
         message = event["message"]
-        sender = event["sender"]
+        sender = event["sender_name"]
+        room_name = event["room_name"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message, "sender": sender}))
+        await self.send(text_data=json.dumps({"message": message, "sender_name": sender, "room_name": room_name}))
+    
+    def update_message_db(self, message, sender_name, room_name):
+        message_serializer = MessageSerializer(data= {
+            "message": message,
+            "sender_name": sender_name,
+            "room_name": room_name
+        })
+
+        if message_serializer.is_valid():
+            message_serializer.save()
