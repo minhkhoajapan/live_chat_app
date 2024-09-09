015ec69d830c7aae92eabdaa9ddc1be9b95bdb5e
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-09 18:02:08 +0900

    finish deserialization test for Message model

--- backend/chat/tests/test_serializer.py
+++ backend/chat/tests/test_serializer.py
@@ -1,8 +1,7 @@
 import pytest
 from django.contrib.auth.models import User
-from ..models import Message
-from ..serializers import MessageSerializer
-from datetime import datetime
+from chat.models import Message
+from chat.serializers import MessageSerializer
 
 @pytest.fixture
 def test_user() -> User:
@@ -12,20 +11,32 @@ def test_user() -> User:
 def another_user() -> User:
     return User.objects.create_user(username="anotheruser", password="Testpassword123!")
 
+@pytest.mark.django_db
 @pytest.mark.parametrize(
-    "message, room_name, user_fixture",
+    "message, room_name, sender_name, user_fixture",
     [
-        ("Hello", "room1", "test_user"),
-        ("Hi friend, room2, another_user"),
-        ("Hi again", "room3", "test_user")
+        ("Hello", "room1", "testuser", "test_user"),
+        ("Hi friend", "room2", "anotheruser", "another_user"),
+        ("Hi again", "room3", "testuser", "test_user")
     ],
 )
-def test_message_deserialization(request, message, room_name, user_fixture):
+def test_message_deserialization(request, message, room_name, sender_name, user_fixture):
     #Dynamically get the user fixture (test_user or another_user0)
     user: User = request.getfixturevalue(user_fixture)
 
     data = {
         'message': message,
         'room_name': room_name,
-        'sender': user.username
+        'sender_name': sender_name
     }
+
+    serializer = MessageSerializer(data=data)
+    assert serializer.is_valid()
+
+    #save the deserialized instance
+    message_instance = serializer.save()
+
+    assert message_instance.message == message
+    assert message_instance.sender == user
+    assert message_instance.room_name == room_name
+
