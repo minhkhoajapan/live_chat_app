248198d3cf1f3c5fd5aaef0041bd0928c0f305a6
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-12 14:28:41 +0900

    add api endpoint path for creating chat room

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PreloadMessage
+from .views import PreloadMessage, CreatChatRoomView
 
 urlpatterns = [
-    path("load/messages/<str:room_name>/", PreloadMessage.as_view(), name="preload messages")
+    path("load/messages/<str:room_name>/", PreloadMessage.as_view(), name="preload messages"),
+    path("create/chatroom/", CreatChatRoomView.as_view(), name="create_chat_room"),
 ]
