62de0bee42e057a6d30c68cf2c1d5bcc2170523a
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-12 14:19:08 +0900

    slight changes in api path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PreloadMessage
 
 urlpatterns = [
-    path("<str:room_name>/", PreloadMessage.as_view(), name="preload messages")
+    path("load/messages/<str:room_name>/", PreloadMessage.as_view(), name="preload messages")
 ]
