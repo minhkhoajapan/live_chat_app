2a4d32a854b9c3d3ab1764e6cfd8d15ee9c10b8b
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-14 00:36:29 +0900

    add user_in_chat_room api end point url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PreloadMessage, CreatChatRoomView
+from .views import PreloadMessage, CreatChatRoomView, UserInChatRoomValidation
 
 urlpatterns = [
     path("load/messages/<str:room_name>/", PreloadMessage.as_view(), name="preload messages"),
     path("create/chatroom/", CreatChatRoomView.as_view(), name="create_chat_room"),
+    path("validation/user/chatroom/", UserInChatRoomValidation.as_view(), name="user_in_chat_room")
 ]
