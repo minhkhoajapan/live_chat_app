780b0d9b8f6061868f72ae1cc38fbf66e9f626df
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-14 09:46:47 +0900

    complete and passed testing for room that does not exist

--- backend/api/test/test_InChatRoom.py
+++ backend/api/test/test_InChatRoom.py
@@ -71,3 +71,35 @@ def test_user_in_room_fail(usernames, userpasswords, room_name, password, outsid
     for username in outside_usernames:
         response: Response = api_client.post(check_user_in_room_url, data={'username': username, 'room_name': room_name})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "usernames, userpasswords, room_name, password",
+    [
+        (["randomuser1", "randomuser2", "randomuser3"], ["randompass1", "randompass2", "randompass3"], "randomroom", "randomroompass"),
+    ],
+)
+def test_response_chat_room_not_exist(usernames, userpasswords, room_name, password):
+    create_room_url = reverse('create_chat_room')
+    users = [User.objects.create_user(username=username, password=userpassword) for username, userpassword in zip(usernames, userpasswords)]
+    room_data = {
+        'room_name': room_name, 
+        'password': password,
+        'usernames': usernames
+    }
+
+    #choose the first user to be the room chat creater
+    api_client = APIClient()
+    refresh_token = RefreshToken.for_user(users[0])
+    access_token = str(refresh_token.access_token)
+    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
+    #create room with three users in the room
+    api_client.post(create_room_url, data=room_data)
+
+    #make api call to check a user in chat room or not
+    check_user_in_room_url = reverse('user_in_chat_room')
+    for username in usernames:
+        response: Response = api_client.post(check_user_in_room_url, data={'username': username, 'room_name': 'none_existing_room'})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['detail'] == 'This chat room does not exist.'
