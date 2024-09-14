76e147c44cbe4ba3473511c7d70bf0f4d5c4a051
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-09-14 16:39:24 +0900

    wrap ChatRoom in ProtectedChatRoom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import react from "react"
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import ChatRoom from "./components/Chatroom"
+import ProtectedChatRoom from "./components/ProtectedChatRoom"
 import ProtectedRoute from "./components/ProtectedRoute"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
@@ -15,7 +16,7 @@ function App() {
         <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/chat/:roomName" element={<ProtectedRoute><ChatRoom /></ProtectedRoute>} />
+        <Route path="/chat/:roomName" element={<ProtectedRoute><ProtectedChatRoom><ChatRoom /></ProtectedChatRoom></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   )
