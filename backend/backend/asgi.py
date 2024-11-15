259ed11a7d28048e3cd9c16bf1622f22cf342c6a
NGUYEN MINH KHOA <minhkhoajapan0701@gmail.com>
2024-11-16 00:06:57 +0900

    Fix module import paths for nested backend

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 django_asgi_app = get_asgi_application()
 
 from chat.routing import websocket_urlpatterns
